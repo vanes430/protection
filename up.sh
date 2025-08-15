3a86d55401cc44a26f01bbe7c6eea86d69a2f128
Shimazu <71627068+vanes430@users.noreply.github.com>
2025-08-15 14:40:31 +0000

    improve the detection

--- up.sh
+++ up.sh
@@ -1,8 +1,7 @@
+docker build -f graaljdk24/Containerfile -t ghcr.io/vanes430/protection:java_graaljdk24 .
+docker build -f graaljdk21/Containerfile -t ghcr.io/vanes430/protection:java_graaljdk21 .
 docker build -f 21/Containerfile -t ghcr.io/vanes430/protection:java_21 .
-docker push ghcr.io/vanes430/protection:java_21
 docker build -f 17/Containerfile -t ghcr.io/vanes430/protection:java_17 .
-docker push ghcr.io/vanes430/protection:java_17
 docker build -f 11/Containerfile -t ghcr.io/vanes430/protection:java_11 .
-docker push ghcr.io/vanes430/protection:java_11
 docker build -f 8/Containerfile -t ghcr.io/vanes430/protection:java_8 .
-docker push ghcr.io/vanes430/protection:java_8
+docker push --all-tags ghcr.io/vanes430/protection
