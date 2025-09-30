ef5b287d913e161d0c23017c44f7e0fb8ae90b5d
Shimazu <71627068+vanes430@users.noreply.github.com>
2025-09-30 03:29:56 +0000

    graal25

--- up.sh
+++ up.sh
@@ -1,3 +1,4 @@
+docker build -f graaljdk25/Containerfile -t ghcr.io/vanes430/protection:java_graaljdk25 .
 docker build -f graaljdk24/Containerfile -t ghcr.io/vanes430/protection:java_graaljdk24 .
 docker build -f graaljdk21/Containerfile -t ghcr.io/vanes430/protection:java_graaljdk21 .
 docker build -f 21/Containerfile -t ghcr.io/vanes430/protection:java_21 .
